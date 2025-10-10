378c9355202a8f3d7c170d26b8d1a8dd239bde76
egeyardimci <ege.yardimci@sabanciuniv.edu>
2025-10-10 14:20:17 +0300

    fix(backend): fixed issue with cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import process from 'process';
 import { getAllProducts } from './services/productService.js';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
+
+app.get('/products', async (req, res) => {
   res.send(await getAllProducts());
 });
 
