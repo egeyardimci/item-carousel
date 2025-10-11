a3070ceea14d4fff21fd99ba520e663e3b9dc83a
egeyardimci <ege.yardimci@sabanciuniv.edu>
2025-10-11 15:19:00 +0300

    fix(backend): gold prices changes from ounce to gram

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,6 +1,7 @@
 export const fetchGoldPrice = async () => {
   const response = await fetch('https://query1.finance.yahoo.com/v8/finance/chart/GC=F');
   const data = await response.json();
-  const price = data.chart.result[0].meta.regularMarketPrice;
+  let price = data.chart.result[0].meta.regularMarketPrice;
+  price = price / 31.1035; // Convert to grams price
   return price;
 }
